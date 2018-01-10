ed82e814afdbbc18a29ddc356e61575ab22b0b00
barufa <you@example.com>
2018-01-10 12:18:45 -0300

    Actualizando archivos

--- client.erl
+++ client.erl
@@ -1,30 +1,52 @@
 -module(client).
 -compile(export_all).
--export([init/1]).
+-export([init/0]).
 
-username(Server)->%%Lee un nombre de usuario
-	Nombre = io:get_line("Ingrese su nombre de usuario: "),
-	Server ! {new,self(),Nombre},
+-define(PUERTO, 8000).
+-define(HOST, {127, 0, 0, 1}).
+
+username(Socket)->%%Lee un nombre de usuario
+	Nombre = string:strip(io:get_line("Ingrese un nombre de usuario: "), right, $\n),
+	gen_tcp:send(Socket,"NEW/"++Nombre),
 	receive
-		used -> io:format("El usuario ~p ya esta en uso~n",[Nombre]),
-				username(Server);
-		ok   -> io:format("Bienvenido ~p~n",[Nombre])
+		{tcp,Socket,<<"USED/">>}   ->
+			io:format("El usuario ~p ya esta en uso~n",[Nombre]),
+			username(Socket);
+		{tcp,Socket,<<"OKNAME/">>} ->
+			io:format("Bienvenido ~p~n",[Nombre]),
+			Nombre
 	end.
 	
-send(Server,Pid) -> %%Lee una linea y envia el mensaje al servidor
-	case io:get_line("") of
-		"Chat:Salir\n" -> Server!{ex,Pid};
-		Mensaje        -> Server!{msg,Pid,Mensaje},send(Server,Pid)
-	end.%%Se llama recursivamente (asumo que io:get_line duerme hasta que el usuario escriba algo)
-	
-reader()->%%Encargado de recibir los mensajes(Hilo principal para mantener el inbox)
-	receive
-		{msg,Texto} -> io:format("~p~n",[Texto]),
-					   reader();
-		_           -> reader()
+reader(Server,Nombre)->
+	case string:strip(io:get_line(Nombre++": "), right, $\n) of
+		"Chat:Salir" -> Server!ex;
+		Mensaje      -> Server!{msm,Mensaje},reader(Server,Nombre)
 	end.
 
-init(Server)->
-	username(Server),
-	spawn(?MODULE,send,[Server,self()]),
-	reader().
+init()-> main(?PUERTO,?HOST).
+
+main(Puerto,Host)->
+	Opciones = [{active, true}, {mode, binary}],
+	{ok,Socket} = gen_tcp:connect(Host, Puerto, Opciones),
+	io:format("Se conecto al servidor ~w en el puerto ~w~n", [Host, Puerto]),
+	Nombre = username(Socket),
+	Pid=spawn(?MODULE,interfaz,[Socket]),
+	reader(Pid,Nombre).
+	
+interfaz(Socket)->
+	receive
+		ex ->
+			io:format("Interfaz llego ~p~n",[ex]),
+			gen_tcp:send(Socket,<<"EXIT/">>),
+			gen_tcp:close(Socket);
+		{msm,Mensaje}->
+			io:format("Interfaz llego ~p a ~p ~n",[{msm,Mensaje},Socket]),
+			gen_tcp:send(Socket,"MSM/"++Mensaje),
+			interfaz(Socket);
+		{tcp,Socket,<<"MSM/",Texto/binary>>}->
+			io:format("Interfaz llego ~p~n",[{tcp,Texto}]),
+			io:format("~p~n",[binary:bin_to_list(Texto)]),
+			interfaz(Socket);
+		Error->
+			io:format("Error: ~p~n",Error)
+	end.
--- server.erl
+++ server.erl
@@ -1,38 +1,98 @@
-%%Falta agregar todo TCP
 -module(server).
 -compile(export_all).
 -export([init/0]).
 
-return() -> ok.
-return(A) -> A. %%Para enfatizar donde termina la funcion
+-define(PUERTO,8000).
 
-update(Pid,Nombre,L)->%%Actualiza la lista L
-	case lists:any(fun({_P,N}) -> N==Nombre end,L) of
-		true  -> Pid!used,
-				 return(L);
-	    _     -> Pid!ok,
-	             return(L ++ [{Pid,Nombre}])
+update(List,Pid,Nombre)->%%Agrega un Nombre si esta disponible
+	case lists:any(fun({_Id,N}) -> N==Nombre end,List) of
+		true -> {used,List};
+		_Ok  -> {okname,List++[{Pid,Nombre}]}
 	end.
 
-update(Pid,L)->%%Actualiza la lista L
-	lists:filter(fun({P,_N}) -> P==Pid end,L).
+remove(List,Pid)->%%Elimina un nombre de la lista
+	lists:filter(fun({Id,_N})-> Pid/=Id end,List).
 
-auxiliar({Pid,Nombre},Id,Txt)-> 
+snd({_A,B})->B.
+
+fixed(List,Pid,Texto)->%%Acomoda el mensaje para identificar a los usuarios
+	case lists:filter(fun({Id,_N}) -> Id==Pid end,List) of
+		[]    -> "Anonimo: "++Texto;
+		[H|_T] -> snd(H)++": "++Texto
+	end.
+
+auxiliar(Pid,Id,Texto)->%%Envie el mensaje si el Id y el Pid son distintos
 	if Pid==Id -> ok;
-	   true    -> Pid!{msg,Nombre++": "++Txt}
+		true   -> Id!{msm,Texto}
 	end.
+	
 
-server(L)-> %% server(Lista de nombres de {PID,username}).
+server(List)->%%Funcion principal del server, se encarga de responde a los clientes(Hilos de esta maquina)
 	receive
-		{new,Pid,Nombre}  -> A = update(Pid,Nombre,L),
-						     server(A);
-		{msg,Pid,Texto}   -> lists:map(fun(X) -> auxiliar(X,Pid,Texto) end,L),
-							 server(L);
-		{ex,Pid}          -> A = update(Pid,L),
-							 server(A)
+		{new,Pid,Nombre} ->
+			io:format("Recibido ~p por funcion server~n",[Nombre]),
+			{Atom,L}=update(List,Pid,Nombre),
+			Pid!Atom;			
+		{msm,Pid,Texto}  ->
+			L=List,
+			Mensaje=fixed(List,Pid,Texto),
+			lists:map(fun({Id,_N}) -> auxiliar(Pid,Id,Mensaje) end,List);
+		{ex,Pid}         -> 
+			L=remove(List,Pid)
+	end,
+	server(L).
+
+init()->
+	Pid = spawn(fun() -> server([]) end),
+	spawn(?MODULE,main,[?PUERTO,Pid]).
+
+main(Puerto,Server)->%%Crea un socket y va a esperar conexiones
+	Opciones = [{active, true}, {mode, binary}],
+	{ok,Socket} = gen_tcp:listen(Puerto,Opciones),
+	io:format("Escuchando en el puetro ~p~n",[Puerto]),
+	listen(Socket,Server).
+	
+listen(Socket,Server)->%%Espera nuevas conexiones
+	{ok,Newsocket} = gen_tcp:accept(Socket),
+	spawn(fun() -> listen(Socket,Server) end),
+	io:format("Se ha establecido una nueva conexio ~p~n",[Newsocket]),
+	cliente(Newsocket,Server).
+
+cliente(Socket,Server)->%%Interface entre el cliente y el server(Simplifica tcp)
+	receive
+	%%Enviado por server
+		used        ->
+			io:format("Recibido used~n"),
+			gen_tcp:send(Socket,"USED/"),
+			cliente(Socket,Server);
+		okname      ->
+			io:format("Recibido okname~n"),
+			gen_tcp:send(Socket,"OKNAME/"),
+			cliente(Socket,Server);
+		{msm,Texto} ->
+			io:format("Recibido msm con ~p a ~p ~n",[Texto,Socket]),
+			gen_tcp:send(Socket,"MSM/"++Texto),
+			io:format("Mensaje enviado~n"),
+			cliente(Socket,Server);
+	%%Enviado por el cliente
+		{tcp,Socket,<<"NEW/",Nombre/binary>>} ->
+			io:format("Nuevo usuario ~p~n",[Nombre]),
+			Server!{new,self(),binary:bin_to_list(Nombre)},
+			cliente(Socket,Server);
+		{tcp,Socket,<<"MSM/",Texto/binary>>}  ->
+			io:format("Nuevo mensaje ~p~n",[Texto]),
+			Server!{msm,self(),binary:bin_to_list(Texto)},
+			cliente(Socket,Server);
+		{tcp,Socket,"EXIT/"}                  ->
+			io:format("Se recibio Exit~n",[]),
+			Server!{ex,self()},
+			gen_tcp:close(Socket);
+		{tcp,Socket,Datos}                    ->
+			io:format("Se recibio ~p~n",[Datos]),
+			cliente(Socket,Server);
+		{tcp_closed, Socket}                  ->
+			io:format("Se cerro la conexion~n");
+	%%Errores
+		Mensaje                    ->
+			io:format("Error: ~p~n",[Mensaje]),cliente(Socket,Server)
 	end.
-
-init()-> 
-	io:format("~p~n",[self()]),
-	server([]).
-
